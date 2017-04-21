f4ea60a07716a759127aae1c6a896225c4c4ef05
claudiumarcutiu <clau.marcutiu.cm@gmail.com>
2017-04-21 13:01:48 +0300

    Add files via upload

---
+++ JoystickSample/canvas.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace JoystickSample
+{
+    partial class canvas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bTrimite = new System.Windows.Forms.Button();
+            this.tbProgress = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonStratCalibration = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonStopCalibration = new System.Windows.Forms.Button();
+            this.tbFrecare = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bResetCanvas = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(613, 418);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // bTrimite
+            // 
+            this.bTrimite.Location = new System.Drawing.Point(650, 25);
+            this.bTrimite.Name = "bTrimite";
+            this.bTrimite.Size = new System.Drawing.Size(131, 57);
+            this.bTrimite.TabIndex = 1;
+            this.bTrimite.Text = "Trimite date";
+            this.bTrimite.UseVisualStyleBackColor = true;
+            this.bTrimite.Click += new System.EventHandler(this.bTrimite_Click);
+            // 
+            // tbProgress
+            // 
+            this.tbProgress.Location = new System.Drawing.Point(23, 19);
+            this.tbProgress.Name = "tbProgress";
+            this.tbProgress.Size = new System.Drawing.Size(100, 20);
+            this.tbProgress.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.tbProgress);
+            this.groupBox1.Location = new System.Drawing.Point(640, 191);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(154, 49);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Progres";
+            // 
+            // buttonStratCalibration
+            // 
+            this.buttonStratCalibration.Location = new System.Drawing.Point(6, 16);
+            this.buttonStratCalibration.Name = "buttonStratCalibration";
+            this.buttonStratCalibration.Size = new System.Drawing.Size(75, 23);
+            this.buttonStratCalibration.TabIndex = 5;
+            this.buttonStratCalibration.Text = "Start";
+            this.buttonStratCalibration.UseVisualStyleBackColor = true;
+            this.buttonStratCalibration.Click += new System.EventHandler(this.buttonStratCalibration_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox2.Controls.Add(this.buttonStopCalibration);
+            this.groupBox2.Controls.Add(this.buttonStratCalibration);
+            this.groupBox2.Location = new System.Drawing.Point(624, 287);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(170, 52);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Calibrare";
+            // 
+            // buttonStopCalibration
+            // 
+            this.buttonStopCalibration.Location = new System.Drawing.Point(87, 16);
+            this.buttonStopCalibration.Name = "buttonStopCalibration";
+            this.buttonStopCalibration.Size = new System.Drawing.Size(75, 23);
+            this.buttonStopCalibration.TabIndex = 6;
+            this.buttonStopCalibration.Text = "Stop";
+            this.buttonStopCalibration.UseVisualStyleBackColor = true;
+            this.buttonStopCalibration.Click += new System.EventHandler(this.buttonStopCalibration_Click);
+            // 
+            // tbFrecare
+            // 
+            this.tbFrecare.Location = new System.Drawing.Point(681, 392);
+            this.tbFrecare.Name = "tbFrecare";
+            this.tbFrecare.Size = new System.Drawing.Size(100, 20);
+            this.tbFrecare.TabIndex = 7;
+            this.tbFrecare.TextChanged += new System.EventHandler(this.tbFrecare_TextChanged);
+            this.tbFrecare.MouseLeave += new System.EventHandler(this.tbFrecare_MouseLeave);
+            this.tbFrecare.MouseHover += new System.EventHandler(this.tbFrecare_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(695, 376);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Coef. Frecare";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bResetCanvas
+            // 
+            this.bResetCanvas.Location = new System.Drawing.Point(650, 96);
+            this.bResetCanvas.Name = "bResetCanvas";
+            this.bResetCanvas.Size = new System.Drawing.Size(131, 63);
+            this.bResetCanvas.TabIndex = 9;
+            this.bResetCanvas.Text = "Resetare";
+            this.bResetCanvas.UseVisualStyleBackColor = true;
+            this.bResetCanvas.Click += new System.EventHandler(this.bResetCanvas_Click);
+            // 
+            // canvas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 424);
+            this.Controls.Add(this.bResetCanvas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFrecare);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bTrimite);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "canvas";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.canvas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bTrimite;
+        private System.Windows.Forms.TextBox tbProgress;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonStratCalibration;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonStopCalibration;
+        private System.Windows.Forms.TextBox tbFrecare;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bResetCanvas;
+    }
+}
---
+++ JoystickSample/canvas.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+namespace JoystickSample
+{
+    public partial class canvas : Form
+    {
+        private frmMain form = null;
+        List<System.Drawing.Point> coordonates = new List<System.Drawing.Point>(); //Lista pentru coordonatele clickurilor
+        int clickCount = 0; //numara de cate ori se da click pe pictureBox1
+        Graphics G;
+        Pen p = new Pen(Color.Black, 2);
+        int lengthsIndex = 0;
+        int anglesIndex = 0;
+        List<int> lengths = new List<int>();
+        List<double> angles = new List<double>();
+        byte[] byteArray = new byte[4];
+
+        Stopwatch stopwatch = new Stopwatch();
+        double calibrationTime;
+        double tempEditedTime;
+        bool stopwatchFlag = false;
+
+        public canvas(Form callingForm)
+        {
+            InitializeComponent();
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            form = callingForm as frmMain; //referinta la frmMain
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height); //Setam un bitmap ca sursa la pictureBox1
+            G = Graphics.FromImage(pictureBox1.Image); //se seteaza G = ... pentru a putea desena linii pe pictureBox1
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs em = (MouseEventArgs)e;
+            coordonates.Add((System.Drawing.Point)em.Location); //coordonatele de click in pictureBox1
+            clickCount++; //creste contorul
+            //Debug.Write("\n"+thisPosition.X + "    " + thisPosition.Y+ "\n");
+            if (clickCount > 1) //daca avem cel putin 3 coordonate in lista
+            {
+                G.DrawLine(p, coordonates[clickCount - 2], coordonates[clickCount - 1]); //desenz linia dintre ultimele 2 clickuri
+
+                pictureBox1.Invalidate();
+                if (clickCount > 2)
+                {
+                    double sideOne, sideTwo, sideThree;
+                    System.Drawing.Point first, second, third;
+                    first = coordonates[clickCount - 3];
+                    second = coordonates[clickCount - 2];
+                    third = em.Location;
+                    sideOne = Math.Sqrt(getSquare(second.X - first.X) + getSquare(second.Y - first.Y));
+                    sideTwo = Math.Sqrt(getSquare(third.X - second.X) + getSquare(third.Y - second.Y));
+                    sideThree = Math.Sqrt(getSquare(third.X - first.X) + getSquare(third.Y - first.Y));
+
+                    if (clickCount == 3)
+                    {
+                        lengths.Add((int)sideOne);
+                        lengths.Add((int)sideTwo);
+                    }
+                    else
+                    {
+                        lengths.Add((int)sideTwo);
+                    }
+                    double r = (second.X - first.X) * (second.Y - first.Y);
+                    double r1 = (second.X - first.X) * (third.Y - first.Y);
+                    double r2 = (second.X - third.X) * (second.Y - first.Y);
+
+                    double leftOrRight = r - r1 - r2;
+
+                    if (sideTwo < 1)
+                    {
+                        sideTwo = 1;
+                    }
+                    if (sideThree < 1)
+                    {
+                        sideThree = 1;
+                    }
+                    if (sideOne < 1)
+                    {
+                        sideOne = 1;
+                    }
+
+                    double finalAngle = Math.Acos((sideTwo*sideTwo + sideOne*sideOne - sideThree * sideThree) / (2 * sideTwo * sideOne)) * 180/Math.PI;
+
+                    
+
+                    int l1, m1, l2, m2;
+                    l1 = second.X - (-1 * first.X);
+                    m1 = second.Y - first.Y;
+                    l2 = third.X - second.X;
+                    m2 = third.Y - second.Y;
+
+                    double finalAngle2 = Math.Acos((l1 * l2 + m1 * m2) / (Math.Sqrt(l1 * l1 + m1 * m1) * Math.Sqrt(l2 * l2 + m2 * m2)));
+                    if (leftOrRight < 0)
+                    {
+                        finalAngle = -finalAngle;
+                    }
+                    angles.Add(finalAngle);
+                    Debug.Write(finalAngle.ToString() + "   " + leftOrRight + "\n");
+
+                    //}
+                }
+            }
+        }
+
+        private double getSquare(double value)
+        {
+            return Math.Pow(value, 2.0);
+        }
+
+        private void bTrimite_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = new ThreadStart(processValues);
+            Thread t = new Thread(ts);
+            calibrationTime = float.Parse(tbFrecare.Text);
+            t.Start();
+        }
+
+        public void updateTextbox(String text)
+        {
+            MethodInvoker action = delegate
+            { tbProgress.Text = text; };
+            tbProgress.BeginInvoke(action);
+            //tbProgress.Text = text;
+        }
+
+        void processValues()
+        {
+            float percentProgress = 0;
+            double progressValue = 0;
+            updateTextbox(progressValue + "");
+            //tbProgress.Text = percentProgress + " %";
+            //int limit = angles.Count + lengths.Count;
+            List<double> allItems = new List<double>();
+            for (int i = 0; i < angles.Count; i++)
+            {
+                allItems.Add(lengths[i]);
+                allItems.Add(angles[i]);
+                if (i == angles.Count - 1)
+                {
+                    allItems.Add(lengths[i + 1]);
+                }
+            }
+            Boolean isAngle = false;
+            for (int i = 0; i < allItems.Count; i++)
+            {
+                double itemValue = 100 / allItems.Count;
+                double x = allItems[i];
+                if (i % 2 == 0)
+                {
+                    //Inseamna ca e latura
+                    //
+                    byteArray[0] = 2;
+                    byteArray[1] = 2 + 60;
+                    byteArray[2] = 2 + 120;
+                    byteArray[3] = 2 + 180;
+                }
+                else
+                {
+                    //Inseamna ca e unghi
+                    if (x < 0)
+                    {
+                        x = -x;
+                        byteArray[0] = (byte)2;
+                        byteArray[1] = (byte)2 + 60;
+                        byteArray[2] = (byte)58 + 120;
+                        byteArray[3] = (byte)58 + 180;
+                    }
+                    else
+                    {
+                        byteArray[0] = (byte)58;
+                        byteArray[1] = (byte)58 + 60;
+                        byteArray[2] = (byte)2 + 120;
+                        byteArray[3] = (byte)2 + 180;
+                    }
+                    isAngle = true;
+
+
+                }
+                if (isAngle)
+                {
+                    //must be an angle
+                    //int time = (int)(8 * x);
+                    if (x < 0)
+                        x = -x;
+                    x = 180 - x;
+                    int time = (int)(calibrationTime * x / 360);
+                    form.form2Send(byteArray);
+                    try
+                    {
+                        Thread.Sleep(time);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Write(e.Message);
+                    }
+                }
+                else
+                {
+                    //must be a line
+                    form.form2Send(byteArray);
+                    int time = (int)(x * 8);
+                    Thread.Sleep(time);
+                    if (i == allItems.Count - 1)
+                    {
+                        updateTextbox("100");
+                        byteArray[0] = (byte)30;
+                        byteArray[1] = (byte)30;
+                        byteArray[2] = (byte)30;
+                        byteArray[3] = (byte)30;
+                        form.form2Send(byteArray);
+
+                    }
+                }
+                progressValue += itemValue;
+                if (i != allItems.Count - 1)
+                    updateTextbox("" + progressValue);
+                isAngle = false;
+            }
+        }
+
+        private void buttonStratCalibration_Click(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+            byteArray[0] = (byte)58;
+            byteArray[1] = (byte)58 + 60;
+            byteArray[2] = (byte)2 + 120;
+            byteArray[3] = (byte)2 + 180;
+            form.form2Send(byteArray);
+            calibrationTime = 0;
+            stopwatchFlag = true;
+        }
+
+        private void buttonStopCalibration_Click(object sender, EventArgs e)
+        {
+            if (stopwatchFlag)
+            {
+                stopwatch.Stop();
+                byteArray[0] = (byte)30;
+                byteArray[1] = (byte)30;
+                byteArray[2] = (byte)30;
+                byteArray[3] = (byte)30;
+                form.form2Send(byteArray);
+                calibrationTime = stopwatch.Elapsed.TotalMilliseconds / 5;
+                tbFrecare.Text = "" + calibrationTime;
+                tempEditedTime = calibrationTime;
+                stopwatchFlag = false;
+            }
+        }
+
+        private void tbFrecare_MouseHover(object sender, EventArgs e)
+        {
+            //tbFrecare.Text = "" + calibrationTime;
+        }
+
+        private void tbFrecare_TextChanged(object sender, EventArgs e)
+        {
+            // if (tbFrecare.Text != "" && tbFrecare.Text != ""+calibrationTime)
+            // tempEditedTime = float.Parse(tbFrecare.Text);
+        }
+
+        private void canvas_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void canvas_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbFrecare_MouseLeave(object sender, EventArgs e)
+        {
+            // tbFrecare.Text = "" + tempEditedTime;
+        }
+
+        private void bResetCanvas_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height); //Setam un bitmap ca sursa la pictureBox1
+            G = Graphics.FromImage(pictureBox1.Image); //se seteaza G = ... pentru a putea desena linii pe pictureBox1
+            lengths.Clear();
+            angles.Clear();
+            clickCount = 0;
+            coordonates.Clear();
+        }
+
+        private void canvas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
